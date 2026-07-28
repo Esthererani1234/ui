cde4ee65d5aec288845faa8491137c69e34ac70b
Esthererani1234 <esthererani14@gmail.com>
2026-07-28 00:32:04 -0400

    Speed up storefront data and images

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./state/AuthContext";
 import { CartProvider } from "./state/CartContext";
+import { fetchMarketPrices } from "./lib/marketPrices";
+import { fetchActiveProducts } from "./lib/catalogCache";
 import "./styles.css";
+
+// Start public storefront data requests before React mounts. Components reuse
+// these shared in-flight requests instead of beginning serial waits later.
+fetchMarketPrices().catch(() => {});
+fetchActiveProducts().catch(() => {});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
